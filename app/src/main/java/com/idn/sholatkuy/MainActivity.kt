1923f1d7212d947e94ce58bc6d1c3a426aca4ac6
Rifky Syaripudin <rks@jiwalu.id>
2022-05-30 21:31:20 +0700

    Initial commit

--- app/src/main/java/com/idn/sholatkuy/MainActivity.kt
+++ app/src/main/java/com/idn/sholatkuy/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.idn.sholatkuy
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonNyoba: Button = findViewById(R.id.btn_nyoba)
+        buttonNyoba.setOnClickListener{
+            startActivity(Intent(this, QiblatActivity::class.java))
+        }
     }
+
 }
--- app/src/main/java/com/idn/sholatkuy/QiblatActivity.kt
+++ app/src/main/java/com/idn/sholatkuy/QiblatActivity.kt
@@ -1,11 +1,64 @@
 package com.idn.sholatkuy
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.RotateAnimation
+import android.widget.ImageView
 
-class QiblatActivity : AppCompatActivity() {
+class QiblatActivity : AppCompatActivity(), SensorEventListener {
+
+    private var currentDegree = 0f
+
+    private var mSendorManager: SensorManager? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_qiblat)
+        initData()
     }
+
+    private fun initData() {
+        mSendorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager?
+    }
+
+    override fun onResume() {
+        super.onResume()
+        @Suppress("DEPRECATION")
+        mSendorManager?.registerListener(this,
+            mSendorManager?.getDefaultSensor(Sensor.TYPE_ORIENTATION),
+            SensorManager.SENSOR_DELAY_GAME)
+    }
+
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        val degree = Math.round(event?.values?.get(0)!!)
+
+        val rotateAnimation = RotateAnimation(
+            currentDegree,
+            (-degree).toFloat(),
+            Animation.RELATIVE_TO_SELF,
+                0.5f,
+            Animation.RELATIVE_TO_SELF,
+                0.5f
+        )
+
+        rotateAnimation.duration = 210
+        rotateAnimation.fillAfter = true
+
+        var imgArrow = findViewById<ImageView>(R.id.imgCompass)
+
+        imgArrow.startAnimation(rotateAnimation)
+        currentDegree = (-degree).toFloat()
+    }
+
+
 }
