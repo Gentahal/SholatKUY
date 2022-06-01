3ed21fc2c3e5ccf780c542e55de4ab834f01d450
AbdiRabbani <abdirabbani59@gmail.com>
2022-06-01 14:09:50 +0700

    initial commit

--- app/src/main/java/com/idn/sholatkuy/MainActivity.kt
+++ app/src/main/java/com/idn/sholatkuy/MainActivity.kt
@@ -2,8 +2,13 @@ package com.idn.sholatkuy
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 import com.idn.sholatkuy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(){
@@ -11,25 +16,44 @@ class MainActivity : AppCompatActivity(){
     private var _binding: ActivityMainBinding? = null
     private val binding get() = _binding as ActivityMainBinding
 
+    lateinit var toggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Hide the status bar.
-        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
-        // Remember that you should never show the action bar if the
-        // status bar is hidden, so hide that too if necessary.
-        actionBar?.hide()
-
         binding.btnProfile.setOnClickListener {
             startActivity(Intent(this@MainActivity, ProfileActivity::class.java))
         }
 
+        val drawerLayout : DrawerLayout = binding.drawerLayout
+        val navView : NavigationView = binding.navView
 
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
 
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
+        navView.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.navigation_home -> Toast.makeText(applicationContext, "Open", Toast.LENGTH_LONG).show()
+                R.id.navigation_profile -> Toast.makeText(applicationContext, "Open", Toast.LENGTH_LONG).show()
+                R.id.navigation_compas -> Toast.makeText(applicationContext, "Open", Toast.LENGTH_LONG).show()
+            }
+            true
+        }
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)){
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
 
