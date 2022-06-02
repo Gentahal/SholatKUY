42c561304eb0ed63d10907047f5ea856f6a0b008
Rifky Syaripudin <rks@jiwalu.id>
2022-06-02 20:28:41 +0700

    Initial committ

--- app/src/main/java/com/idn/sholatkuy/SholatAdapter.kt
+++ app/src/main/java/com/idn/sholatkuy/SholatAdapter.kt
@@ -5,27 +5,26 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.idn.sholatkuy.databinding.RowItemSholatBinding
 
-class SholatAdapter : RecyclerView.Adapter<SholatAdapter.MyViewHolder>() {
-
-    private var listSholat = List<JadwalResponse>()
-
-    fun setData(data: List<JadwalResponse>?){
-        if(data == null) return
-        listSholat.clear()
-    }
-
-    class MyViewHolder(val binding: RowItemSholatBinding)
-        : RecyclerView.ViewHolder(binding.root)
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = MyViewHolder(
-        RowItemSholatBinding.inflate(LayoutInflater.from(parent.context),parent,false)
-    )
-
-    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        TODO("Not yet implemented")
-    }
-
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
-}
+//class SholatAdapter : RecyclerView.Adapter<SholatAdapter.MyViewHolder>() {
+//
+//    private var listSholat = List<JadwalResponse>()
+//
+//    fun setData(data: List<JadwalResponse>?){
+//        if(data == null) return
+//    }
+//
+//    class MyViewHolder(val binding: RowItemSholatBinding)
+//        : RecyclerView.ViewHolder(binding.root)
+//
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = MyViewHolder(
+//        RowItemSholatBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+//    )
+//
+//    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+//        TODO("Not yet implemented")
+//    }
+//
+//    override fun getItemCount(): Int {
+//        TODO("Not yet implemented")
+//    }
+//}
