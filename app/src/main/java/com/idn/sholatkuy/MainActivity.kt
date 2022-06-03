b3288c064c2d8329cf8bb588d6e7bb6d5f142491
Rifky Syaripudin <rks@jiwalu.id>
2022-06-03 21:11:36 +0700

    Initial committ

--- app/src/main/java/com/idn/sholatkuy/MainActivity.kt
+++ app/src/main/java/com/idn/sholatkuy/MainActivity.kt
@@ -15,53 +15,15 @@ class MainActivity : AppCompatActivity(){
     private var _binding: ActivityMainBinding? = null
     private val binding get() = _binding as ActivityMainBinding
 
-    lateinit var toggle: ActionBarDrawerToggle
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val drawerLayout : DrawerLayout = binding.drawerLayout
-        val navView : NavigationView = binding.navView
+        supportActionBar?.hide()
 
-        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
-        drawerLayout.addDrawerListener(toggle)
-        toggle.syncState()
 
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-
-        navView.setNavigationItemSelectedListener {
-            when(it.itemId){
-                R.id.navigation_home -> {
-                    startActivity(Intent(this, MainActivity::class.java))
-                    finish()
-                }
-                R.id.navigation_profile -> {
-                    startActivity(Intent(this, ProfileActivity::class.java))
-                    finish()
-                }
-                R.id.navigation_compas -> {
-                    startActivity(Intent(this, QiblatActivity::class.java))
-                    finish()
-                }
-                R.id.navigation_search -> {
-                    startActivity(Intent(this, SearchActivity::class.java))
-                    finish()
-                }
-            }
-            true
-        }
-
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (toggle.onOptionsItemSelected(item)){
-            return true
-        }
-
-        return super.onOptionsItemSelected(item)
     }
 }
 
--- app/src/main/java/com/idn/sholatkuy/QiblatActivity.kt
+++ app/src/main/java/com/idn/sholatkuy/QiblatActivity.kt
@@ -1,7 +1,6 @@
 package com.idn.sholatkuy
 
 import android.content.Context
-import android.content.Intent
 import android.graphics.Color
 import android.hardware.Sensor
 import android.hardware.SensorEvent
@@ -9,78 +8,25 @@ import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MenuItem
 import android.view.animation.Animation
 import android.view.animation.RotateAnimation
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatDelegate
-import androidx.drawerlayout.widget.DrawerLayout
-import com.google.android.material.navigation.NavigationView
-import com.idn.sholatkuy.databinding.ActivityProfileBinding
-import com.idn.sholatkuy.databinding.ActivityQiblatBinding
-import com.idn.sholatkuy.databinding.ActivitySearchBinding
 
 class QiblatActivity : AppCompatActivity(), SensorEventListener {
 
-
-    private lateinit var sensorManager: SensorManager
+    private lateinit var sensorManager: SensorManager // ini codingan buat sensor kiblat ya jangan di apus
     private lateinit var square: ImageView            // tinggal buat button yang bisa move ke qiblat
-
-    lateinit var toggle: ActionBarDrawerToggle
-
-    private var _binding : ActivityQiblatBinding? = null
-    private val binding get() = _binding as ActivityQiblatBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        _binding = ActivityQiblatBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_qiblat)
 
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         square = findViewById(R.id.bg_mecca)
 
         setUpSensorStuff()
-
-        val drawerLayout : DrawerLayout = binding.drawerLayout
-        val navView : NavigationView = binding.navView
-        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
-        drawerLayout.addDrawerListener(toggle)
-        toggle.syncState()
-
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-
-        navView.setNavigationItemSelectedListener {
-            when(it.itemId){
-                R.id.navigation_home -> {
-                    startActivity(Intent(this, MainActivity::class.java))
-                    finish()
-                }
-                R.id.navigation_profile -> {
-                    startActivity(Intent(this, ProfileActivity::class.java))
-                    finish()
-                }
-                R.id.navigation_compas -> {
-                    startActivity(Intent(this, QiblatActivity::class.java))
-                    finish()
-                }
-                R.id.navigation_search -> {
-                    startActivity(Intent(this, SearchActivity::class.java))
-                    finish()
-                }
-            }
-            true
-        }
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (toggle.onOptionsItemSelected(item)){
-            return true
-        }
-
-        return super.onOptionsItemSelected(item)
     }
 
     private fun setUpSensorStuff() {
