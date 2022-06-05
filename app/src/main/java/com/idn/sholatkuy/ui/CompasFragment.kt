4aee8dc91d7c4ffd582346578c7d64bd85138c50
Rifky Syaripudin <rks@jiwalu.id>
2022-06-05 21:25:11 +0700

    Initial committ

--- app/src/main/java/com/idn/sholatkuy/ui/CompasFragment.kt
+++ app/src/main/java/com/idn/sholatkuy/ui/CompasFragment.kt
@@ -1,32 +1,45 @@
 package com.idn.sholatkuy.ui
 
+import android.content.Context.SENSOR_SERVICE
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.content.ContextCompat.getSystemService
 import com.idn.sholatkuy.R
+import com.idn.sholatkuy.databinding.FragmentCompasBinding
 
-class QiblatActivity : AppCompatActivity(), SensorEventListener {
+class CompasFragment : Fragment(), SensorEventListener {
 
     private lateinit var sensorManager: SensorManager // ini codingan buat sensor kiblat ya jangan di apus
-    private lateinit var square: ImageView            // tinggal buat button yang bisa move ke qiblat
+    private lateinit var square: ImageView
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_qiblat)
+    private var _binding  : FragmentCompasBinding?  = null
+    private val binding get() = _binding as FragmentCompasBinding
 
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentCompasBinding.inflate(LayoutInflater.from(context), container, false)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-        square = findViewById(R.id.bg_mecca)
+        square = binding.bgMecca
 
         setUpSensorStuff()
+        return binding.root
     }
 
     private fun setUpSensorStuff() {
-        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        sensorManager = getSystemService(requireContext(), SensorManager::class.java) as SensorManager
 
         sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)?.also {
             sensorManager.registerListener(
@@ -53,7 +66,7 @@ class QiblatActivity : AppCompatActivity(), SensorEventListener {
         }
     }
 
-    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
         return
     }
 
--- app/src/main/java/com/idn/sholatkuy/ui/MainActivity.kt
+++ app/src/main/java/com/idn/sholatkuy/ui/MainActivity.kt
@@ -2,13 +2,19 @@ package com.idn.sholatkuy.ui
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
+import com.idn.sholatkuy.R
 import com.idn.sholatkuy.databinding.ActivityMainBinding
-import com.idn.sholatkuy.databinding.ActivitySearchBinding
 
 class MainActivity : AppCompatActivity(){
 
@@ -18,12 +24,25 @@ class MainActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        _binding = ActivityMainBinding.inflate(layoutInflater)
+        _binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
         setContentView(binding.root)
+        setupBottomNavigationBar()
+    }
 
+    private fun setupBottomNavigationBar() {
         supportActionBar?.hide()
-
-
+        val navView: BottomNavigationView = binding.bottomNavView
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
+        navView.setupWithNavController(navHostFragment.navController)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home,
+                R.id.navigation_compas,
+                R.id.navigation_mushaf
+            )
+        )
+        setupActionBarWithNavController(navHostFragment.navController, appBarConfiguration)
     }
 }
 
