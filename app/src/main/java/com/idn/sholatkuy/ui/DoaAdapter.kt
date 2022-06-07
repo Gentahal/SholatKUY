39dce7c786885d0a0c21f61fd4b53470d55b377f
AbdiRabbani <abdirabbani59@gmail.com>
2022-06-07 10:02:49 +0700

    initial commit

---
+++ app/src/main/java/com/idn/sholatkuy/ui/DoaAdapter.kt
@@ -0,0 +1,28 @@
+package com.idn.sholatkuy.ui
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.idn.sholatkuy.response.DoaItem
+
+class DoaAdapter(doa : DoaItem) : RecyclerView.Adapter<DoaAdapter.MyViewHolder>(){
+    class MyViewHolder(val binding) {
+
+    }
+
+    private val listDoa = ArrayList<DoaItem>()
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DoaAdapter.MyViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: DoaAdapter.MyViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+
+}
